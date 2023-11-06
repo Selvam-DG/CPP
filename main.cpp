243e0011b367fe656d7dce485b4b08182eb17a1c
SELVAM D G <dasariselvam321@gmail.com>
2023-11-06 22:17:40 +0100

    First program

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // included standard library for printing on screen
+
+int main(){ 
+    // statement
+
+    std::cout << "Hello World" ;
+    
+    // cout is Character out
+
+    return 0; // why 0, it tells the OS that the program terminate correctly
+}
