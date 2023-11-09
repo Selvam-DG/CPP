a75631c12783a073f446da970bb7fe661091412f
SELVAM D G <dasariselvam321@gmail.com>
2023-11-09 20:24:30 +0100

    prgram

---
+++ swap_2_values.cpp
@@ -0,0 +1,18 @@
+/*Swapping two variable values*/
+
+#include <iostream>
+
+int main(){
+    int a = 1;
+    int b = 2;
+    int c;
+    c = a;
+    a = b;
+    b = c;
+
+    std :: cout << a << std:: endl << b << std:: endl ; 
+    
+    std :: cout << b;
+
+    return 0;
+}
